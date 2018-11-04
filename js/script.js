3d8fd6d361252df53cd2b582e8a7a5d168401109
Abhishek Mazumder <abhishekmazumder69@gmail.com>
2018-11-04 10:58:59 +0530

    2nd commit

--- js/script.js
+++ js/script.js
@@ -4,9 +4,46 @@ $(document).ready(function(){
     $('.toggler-btn').toggleClass('change');
   });
 
-  
+  /*
+  $(window).scroll(function(){
+    let position = $(this).scrollTop();
 
-  
+    if(position>=718){
+      $(".navbar").addClass("navbar-background");
+      $(".navbar").addClass("fixed-top");
+    }
+    else{
+      $(".navbar").removeClass("navbar-background");
+      $(".navbar").removeClass("fixed-top");
+    }
+  });
+
+  //smooth scroll
+  $(".nav-item a, ,header-link, #back-to-top").click(function(link){
+    link.preventDefault();
+
+    let target = $(this).attr("href");
+
+    $("html, body").stop().animate({
+      scrollTop: $(target).offset().top - 25
+    },3000);
+
+
+
+  });
+
+  //back to top button
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+
+    if (position >= 718) {
+      $('#back-to-top').addClass('scrollTop');
+      
+    }
+    else {
+      $('#back-to-top').removeClass('scrollTop');
+    }
+  })*/
 
 
 //ripples
